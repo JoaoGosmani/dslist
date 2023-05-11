8e9293f13bb0769138844ae2445e19edc0b6d578
Joao Gosmani <joaogosmani@gmail.com>
2023-05-11 16:25:34 -0300

    SQL, projection, GET games by list

--- src/main/java/br/com/joaogosmani/dslist/services/GameService.java
+++ src/main/java/br/com/joaogosmani/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import br.com.joaogosmani.dslist.dto.GameDTO;
 import br.com.joaogosmani.dslist.dto.GameMinDTO;
 import br.com.joaogosmani.dslist.entities.Game;
+import br.com.joaogosmani.dslist.projections.GameMinProjection;
 import br.com.joaogosmani.dslist.repositories.GameRepository;
 
 @Service
@@ -27,6 +28,13 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		return dto;
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		return dto;
 	}
